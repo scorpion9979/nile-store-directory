a1df1089853ec5eec53179bb7c6d861cb95fc9ed
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-04-17 22:06:45 +0300

    Improved search options

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -9,26 +9,13 @@
       </div>
     </div>
     <div class="column is-narrow">
-      <div class="dropdown">
-        <div class="dropdown-trigger">
-          <button class="button is-medium" aria-haspopup="true" aria-controls="dropdown-menu3">
-            <span>Grocery</span>
-            <span class="icon is-medium">
-              <i class="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-          <div class="dropdown-content">
-            <a href="#" class="dropdown-item">Overview</a>
-            <a href="#" class="dropdown-item">Modifiers</a>
-            <a href="#" class="dropdown-item">Grid</a>
-            <a href="#" class="dropdown-item">Form</a>
-            <a href="#" class="dropdown-item">Elements</a>
-            <a href="#" class="dropdown-item">Components</a>
-            <a href="#" class="dropdown-item">Layout</a>
-          </div>
-        </div>
+      <div class="select is-medium">
+        <select>
+          <option>Grocery</option>
+          <option>Books</option>
+          <option>Electronics</option>
+          <option>Entertainment</option>
+        </select>
       </div>
     </div>
   </div>
